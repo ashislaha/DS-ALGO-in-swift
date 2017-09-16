4352a5161a0aefd35de6c0927f019c3e43b3a17e
Ashis Laha <ashis.laha@olacabs.com>
2017-09-16 08:58:35 +0530

    isBST using Range of min max

--- tree_4_is_binaryTree_BST.playground/Contents.swift
+++ tree_4_is_binaryTree_BST.playground/Contents.swift
@@ -39,6 +39,17 @@ class BinaryTree {
         print(root.data ?? -1 , terminator: " ")
         inorder(root: root.right)
     }
+    
+    // method 2 : using range of min and max
+    
+    public func isBSTusingRange(root : Node?, min : Int, max : Int) -> Bool {
+        guard let root = root ,let rootData = root.data else { return true }
+        
+        if rootData > min && rootData < max {
+            return isBSTusingRange(root:root.left, min: min, max:rootData-1) && isBSTusingRange(root:root.right, min: rootData+1,max:max)
+        }
+        return false
+    }
 }
 
 let binaryTree = BinaryTree()
@@ -49,5 +60,6 @@ binaryTree.root?.right?.right = Node(val: 12)
 binaryTree.inorder(root: binaryTree.root)
 
 print(binaryTree.isBST(root: binaryTree.root))
+print(binaryTree.isBSTusingRange(root: binaryTree.root, min: Int.min, max: Int.max))
 
 
